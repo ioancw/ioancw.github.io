b6622e4a4613d86d0e27cd640b00480d0e3d7d1e
ioancw <ioan.williams@icloud.com>
2020-07-25 12:43:18 +0100

    latest changes to blog

--- code/2020-07-24-fsharp-literate.fsx
+++ code/2020-07-24-fsharp-literate.fsx
@@ -6,7 +6,7 @@ Up until now, I've mostly written my blog posts directly in markdown, and includ
 snippets copied from fsx files.
 
 Literate programming has existed for a while in the F# ecosystem, although I always found
-it clunky to use, probably because it hasn't been updated for a while.  However Don Syme 
+it clunky to use, probably because it hasn't been updated for a while.  However Don Syme
 gave it a refresh last week, which has made it far more usable, especially as a Mac user.
 
 This purpose of this post is to:
@@ -15,7 +15,7 @@ This purpose of this post is to:
 
 My Blog
 -------
-I use GitHub's Jekyll as a 'blog engine'.  This a static blog engine, which means that 
+I use GitHub's Jekyll as a 'blog engine'.  This a static blog engine, which means that
 you write posts in markdown, submit to your repository which the blog engine renders in html.
 
 This is works well and F# code is rendered resonably well.
@@ -23,19 +23,19 @@ This is works well and F# code is rendered resonably well.
 However a few people seems to be using Literate programming and the associated tools in F#
 in order to generate html files directly, and then post to Jekyll.
 
-I tool my workflow from Colin Bull's [blog][cb] post, and associated github repository, 
+I tool my workflow from Colin Bull's [blog][cb] post, and associated github repository,
 although I updated all dlls to the latest version of FSharp.Formatting.
 
-This file demonstrates how to write literate F# script
-files (`*.fsx`) that can be transformed into nice HTML
-using the `literate.fsx` script from the [F# Formatting
-package](http://fsprojects.github.io/FSharp.Formatting).
+FSX
+---
 
-As you can see, a comment starting with double asterisk
-is treated as part of the document and is transformed 
-using Markdown, which means that you can use:
+The recipe that I follow, is to write the post within a F# script file (.fsx).
 
- - Unordered or ordered lists 
+Start with a double asterisk comment, which allows you to write standard Markdown.
+
+As as example, you can write lists:
+
+ - Unordered or ordered lists
  - Text formatting including **bold** and _emphasis_
 
 And numerous other [Markdown][md] features.
@@ -45,88 +45,65 @@ And numerous other [Markdown][md] features.
 
 Writing F# code
 ---------------
-Code that is not inside comment will be formatted as
-a sample snippet (which also means that you can 
-run it in Visual Studio or MonoDevelop).
+Code that is not inside comment will be formatted as a sample snippet
+(which also means that you can run it in Visual Studio Code or editor of your choosing).
 *)
 
 /// The Hello World of functional languages!
-let rec factorial x = 
-  if x = 0 then 1 
-  else x * (factorial (x - 1))
+let rec factorial x =
+    if x = 0 then 1 else x * (factorial (x - 1))
 
 let f10 = factorial 10
 
-(**
-Hiding code
------------
-
-If you want to include some code in the source code, 
-but omit it from the output, you can use the `hide` 
-command.
+(** 
+You can then write more blog content.  And also some more code:
 *)
 
-(*** hide ***)
-/// This is a hidden answer
-let hidden = 42
+let rec listToString l = 
+  match l with
+  | [] -> ""
+  | head :: tail -> head.ToString() + listToString tail
 
 (** 
-The value will be deffined in the F# code and so you
-can use it from other (visible) code and get correct
-tool tips:
-*)
-
-let answer = hidden
-
-(** 
-Moving code around
-------------------
-
-Sometimes, it is useful to first explain some code that
-has to be located at the end of the snippet (perhaps 
-because it uses some definitions discussed in the middle).
-This can be done using `include` and `define` commands.
-
-The following snippet gets correct tool tips, even though
-it uses `laterFunction`:
-*)
-
-(*** include:later-bit ***)
-
-(**
-Then we can explain how `laterFunction` is defined:
-*)
-
-let laterFunction() = 
-  "Not very difficult, is it?"
-
-(**
-This example covers pretty much all features that are 
-currently implemented in `literate.fsx`, but feel free 
-to [fork the project on GitHub][fs] and add more 
-features or report bugs!
-
-  [fs]: https://github.com/fsprojects/FSharp.Formatting
-
-*)
-
-(*** define:later-bit ***)
-let sample = 
-  laterFunction()
-  |> printfn "Got: %s"
-
-(**
-
-Other features
+Posting to blog
 --------------
 
-The tool-tips also work for double-backtick identifiers.
-This might be useful to generate nice documents from tests:
+Before posting to my blog repository on github, we need to convert from fsx to a html file.
+This can be achieved through FSharp.Formatting.Literate as such.
 
+Create a new fsx file with the following content.
 *)
-let ``1 + 1 should be equal to 2``() =
-  1 + 1 = 2
 
-(**
-Others examples follow here.
+#I "../tools"
+#r "FSharp.Formatting.Common.dll"
+#r "FSharp.Formatting.Markdown.dll"
+#r "FSharp.Formatting.Literate.dll"
+#r "FSharp.Formatting.CodeFormat.dll"
+
+open FSharp.Formatting.Literate
+
+let source = __SOURCE_DIRECTORY__
+
+let codeDirectory = source + "/code"
+let outputDirectory = source + "/_posts"
+
+//process directory, moving files from code to the _posts directory.
+let processDirectory () =
+    Literate.ConvertDirectory(input = codeDirectory, outputDirectory = outputDirectory)
+
+do processDirectory ()
+
+(** 
+This assumes that: 
+* the dlls are in the tools directory
+* fsx files are in the code directory
+* generated html files will be in the _posts directory
+
+The script will process all fsx files in the code directory and create the html files in
+the _posts directory.
+
+You can then run the script from the command line as follows
+``` bash
+dotnet fsi yourscriptname.fsx
+```
 *)
