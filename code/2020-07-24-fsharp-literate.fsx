657b01ce527c851371425ff2e0ae2b1c9fbd18f8
ioancw <ioan.williams@icloud.com>
2020-07-25 14:26:30 +0100

    latest changes to blog

--- code/2020-07-24-fsharp-literate.fsx
+++ code/2020-07-24-fsharp-literate.fsx
@@ -19,31 +19,33 @@ it clunky to use, probably because it hasn't been updated for a while.  However 
 gave it a refresh last week, which has made it far more usable, especially as a Mac user.
 
 This purpose of this post is to:
- - detail how I use Literate within my posts
- - remind me how to use it
+ - detail how I use Literate within my posts.
+ - remind me how to use it, when I forget.
 
 My Blog
 -------
-I use GitHub's Jekyll as a 'blog engine'.  This a static blog engine, which means that
-you write posts in markdown, submit to your repository which the blog engine renders in html.
+I use github's Jekyll as a 'blog engine'.  This a static blog engine, which means that
+you write posts in markdown and submit to your repository which the blog engine renders in html.
 
-This is works well and F# code is rendered resonably well.
+This works well and F# code is rendered resonably neatly (although it's somewhat hard to 
+the colour of the syntax.)
 
-However a few people seems to be using Literate programming and the associated tools in F#
+However a few people seem to be using Literate programming and the associated tools in F#
 in order to generate html files directly, and then post to Jekyll.
 
-I tool my workflow from Colin Bull's [blog][cb] post, and associated github repository,
-although I updated all dlls to the latest version of FSharp.Formatting.
+I based my workflow from Colin Bull's [blog][cb] post, and associated github repository,
+although I updated all dlls to the latest version of FSharp.Formatting.* and used my own 
+scripts and css times.
 
 FSX
 ---
 
-The recipe that I follow, is to write the post within a F# script file (.fsx).
+The recipe that I follow, is to write the post within a F# script file (.fsx) and then 
+conver to html using FSharp.Formatting.Literate.
 
 Start with a double asterisk comment, which allows you to write standard Markdown.
 
 As as example, you can write lists:
-
  - Unordered or ordered lists
  - Text formatting including **bold** and _emphasis_
 
@@ -115,4 +117,5 @@ You can then run the script from the command line as follows
 ``` bash
 dotnet fsi yourscriptname.fsx
 ```
+You dan then write a bash script, in order to commit your files to github.
 *)
