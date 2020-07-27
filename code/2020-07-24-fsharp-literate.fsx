0e09bb2609d76538f08b7bba3f82b89a43561b4b
ioancw <ioan.williams@icloud.com>
2020-07-27 13:27:44 +0100

    latest changes to blog

--- code/2020-07-24-fsharp-literate.fsx
+++ code/2020-07-24-fsharp-literate.fsx
@@ -9,36 +9,47 @@ tags: F# FSharp Literate Formatting
 ---
 
 (**
-Literate F#
+My Blog
+-------
+I use github's Jekyll as a 'blog engine'.  This means I can write posts in markdown 
+and commit to the repository at which point the blog engine renders in nice html.
+
+This works well and F# code is rendered resonably neatly (although it's somewhat hard to 
+change the colour of the syntax).
+
+Jekyll is free, easy enough to use.
+
+However, a few people seem to be using Literate programming and the associated tools in F#
+to generate html files directly, and then post to Jekyll, which it then displayes on your
+blog.
+
+So I decided to give this a crack.  
+
+I based my workflow on Colin Bull's [blog][cb] post, and associated github repository, 
+although I updated all dlls to the latest version of FSharp.Formatting.* and used my 
+own scripts and css files.
+
+Enter Literate F#
 -----------
 
-Up until now, I've written my blog posts directly in markdown, and including code
-snippets copied from fsx files.
+I've been writing my blog posts directly in markdown, and including code
+snippets copied from fsx files, and as stated above, this does seem to work quite well, 
+although it means copying code snippets from other files and icluding them as part of the
+markdown document.
 
 Literate programming has existed for a while in the F# ecosystem, although I always found
 it clunky to use, probably because it hasn't been updated for a while.  However Don Syme
 gave it a complete overhaul last week, which has made it far more usable, especially as a Mac
 user.
 
+The beauty of Literate programming, is that you can write the code and markdown withint the
+same document, e.g. an fsx file.  So you can execute the code directly from the blog post - 
+no need to copy and paste.
+
 This purpose of this post is to:
 
  - detail how I use Literate within my posts.
- - remind me how to use it, when I forget.
-
-My Blog
--------
-I use github's Jekyll as a 'blog engine'.  Which means that posts are written in markdown 
-and submitted to the repository at which point the blog engine renders in nice html.
-
-This works well and F# code is rendered resonably neatly (although it's somewhat hard to 
-change the colour of the syntax.)
-
-However, a few people seem to be using Literate programming and the associated tools in F#
-to generate html files directly, and then post to Jekyll.
-
-So I decided to give this a crack.  I based my workflow on Colin Bull's [blog][cb] post, and 
-associated github repository, although I updated all dlls to the latest version of 
-FSharp.Formatting.* and used my own scripts and css files.
+ - as an aide-memoire when I eventually forget what I did.
 
 FSX
 ---
@@ -46,7 +57,7 @@ FSX
 The recipe that I follow, is to write the post within a F# script file (.fsx) and then 
 convert to html using `FSharp.Formatting.Literate`.
 
-In order to for the Jekyll engine to include the html that's generated into your blog
+In order for the Jekyll engine to include the html that's generated into your blog
 you need to have a 'front matter' section on top of your html, which means that any blog
 post generated from fsx files needs to have the following at the top of the fsx file.
 
@@ -60,8 +71,7 @@ categories:
 tags: F# FSharp Literate
 ---
 ```
-
-As I've done above, it must be after the 'raw' Literate special [command][cd] `(*** raw ***)`
+This must be inserted after the 'raw' Literate special [command][cd] `(*** raw ***)`
 , in order for the Literate processor to ignore it.  If this doesn't exist on your html page, 
 then the Jekyll engine won't include it within your blog.
 
@@ -72,7 +82,7 @@ Start with a double asterisk comment, which allows you to write standard Markdow
 As as example, you can write lists:
 
  - Unordered or ordered lists
- - Text formatting including **bold** and _emphasis_ and ```bash commands``
+ - Text formatting including **bold** and _emphasis_ and ```bash commands```
 
 And numerous other [Markdown][md] features.
 
@@ -83,7 +93,7 @@ And numerous other [Markdown][md] features.
 Writing F# code
 ---------------
 Code that is not inside comment will be formatted as a sample snippet
-(which also means that you can run it in Visual Studio Code or editor of your choosing).
+(which also means that you can run it in Visual Studio Code!).
 
 *)
 
@@ -107,7 +117,7 @@ Posting to blog
 --------------
 
 Before posting to my blog repository on github, we need to convert from fsx to a html file.
-This can be achieved through FSharp.Formatting.Literate as such.
+This can be achieved through FSharp.Formatting.Literate as follows.
 
 Create a new fsx file with the following content.
 *)
@@ -125,7 +135,7 @@ let source = __SOURCE_DIRECTORY__
 let codeDirectory = source + "/code"
 let outputDirectory = source + "/_posts"
 
-//process directory, moving files from code to the _posts directory.
+//process directory, converting files in code to the _posts directory.
 let processDirectory () =
     Literate.ConvertDirectory(input = codeDirectory, outputDirectory = outputDirectory)
 
@@ -145,8 +155,8 @@ You can then run the script from the command line as follows
 ``` bash
 dotnet fsi yourscriptname.fsx
 ```
-You can then include this in a bash script that will run this file and commit to your
-github repository:
+If you wanted a once step, post-to-jekyll process, you could include this in a bash script 
+that will run this file and commit to your github repository:
 ``` bash 
 #!/bin/bash
 dotnet fsi processScript.fsx
@@ -181,9 +191,9 @@ However when creating content for your blog post on Jekyll, you don't need to ap
 As Jekyll already embeds your html within the main page template for your post.  But you
 do need to ensure that the main css file is uploaded to your Jekyll repository.
 
-The best way to visualise this - and the way that I understood it - is to right click in your 
-browser and view the page source.  You should be then able to click on the css file that I 
-used and see yourself what settings I used to prettify the output.
+The best way to visualise this - and the way that made most sense to me - is to right click 
+in your browser and view the page source.  You should be then able to click on the css file 
+that I used and see yourself what settings I used to prettify the output.
 
 I don't come from a web development background, css is new to me, so I found viewing the
 source in the brower and tinkering with the css to be valuable in order to visualise the 
